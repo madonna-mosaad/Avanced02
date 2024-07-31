59492fce313ddeff7d9ddc3192efaad85b9ece28
madonna-mosaad <madonnamosaad86@gmail.com>
2024-08-01 01:29:54 +0300

    part2

--- Avanced02/Program.cs
+++ Avanced02/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace Avanced02
+﻿using System.Collections;
+
+namespace Avanced02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region part2
+            #region Q1
+            //ArrayList arrayList = new ArrayList { 1, "r", true, 2, 3, 4, 5, };
+            //Class1.reverse(ref arrayList);
+            //foreach (var item in arrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #region Q2
+            //List<int> list ;
+            //list = Enumerable.Range(0, 10).ToList();
+            //List<int> result=Class1.EvenNum(list);
+            //foreach (var item in result) 
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+            #region Q3
+            //FixedSizeList<int> fixedSizeList = new FixedSizeList<int>(5);
+            //fixedSizeList.Add(1);
+            //fixedSizeList.Add(2);
+            //fixedSizeList.Add(3);
+            //fixedSizeList.Add(4);
+            //fixedSizeList.Add(5);
+            //fixedSizeList.Add(6);//out of range exception
+            //Console.WriteLine(fixedSizeList.GetValue(0));
+            //Console.WriteLine(fixedSizeList.GetValue(5));//index exception
+            #endregion
+            #region Q4
+            //string str = "mmddp";
+            //Console.WriteLine(Class1.nonReapeted(str));
+            #endregion
+            #endregion
         }
     }
 }
